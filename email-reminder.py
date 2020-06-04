d5a633ef2fcfc5c366d7636c81ccad114464eca7
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-06-03 23:31:34 -0500

    Update email-reminder.py

--- email-reminder.py
+++ email-reminder.py
@@ -9,17 +9,17 @@ from datetime import datetime
 from datetime import date
 from datetime import time, timedelta
 from ima_info import targets, tickers, ima_emails
-import config
+
 #import alpaca_trade_api as tradeapi
 
-sender = #Sender_email
-receiver = #Receiver_email
-password = #password
+sender = #Sender's email
+receiver =  #Receiver's email
+password = #Password
 date_today = date.today()
 
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
-msg['Subject'] =  str(date_today.month)+ '/' + str(date_today.day) + '/' +str(date_today.year)+" || Price Alert"
+msg['Subject'] =  'New Format, Who Dis || '+str(date_today.month)+ '/' + str(date_today.day) + '/' +str(date_today.year)+" || Price Alert"
 msg['From'] = sender
 msg['To'] = ", ".join(receiver)
 
@@ -33,35 +33,37 @@ def func(ticker, target):
     for i in range (len(ticker)):
         price_data = web.DataReader(ticker[i], 'yahoo', start, end)['Adj Close']
         price = round(price_data[-1],2)
-        from_target = (target[i]-price)/price
+        of_target = price/(target[i])
         
         dict_ini[ticker[i]]={}
         dict_ini[ticker[i]]['Price']= price
         dict_ini[ticker[i]]['Target']= target[i]
         dict_ini[ticker[i]]['Move Today'] = str(round(((price_data[-1]-price_data[-2])/price_data[-2])*100,2))+'%'
-        dict_ini[ticker[i]]['% From Target']= round(from_target*100,2)
+        dict_ini[ticker[i]]['% of Target']= round(of_target*100,2)
 
     return dict_ini
 
 info = func(tickers, targets)
 
+# Preparing and cleaning data
 df = pd.DataFrame.from_dict(info)
 df = df.transpose()
 
-indexNames = df[(df['% From Target'] > 3)].index
+indexNames = df[(df['% of Target'] < 97)].index
 df.drop(indexNames , inplace=True)
 
 for i in range(len(df)):
-    if df['% From Target'][i]<=0:
-        df.replace(df['% From Target'][i],'Target Hit',inplace = True)
+    df.replace(df['% of Target'][i],str(df['% of Target'][i])+'%',inplace = True)
+    df.replace(df['Price'][i],'$'+str(df['Price'][i]),inplace = True)
+    df.replace(df['Target'][i],'$'+str(df['Target'][i]),inplace = True)
 
 html = df.to_html()
 
 def email_send():
     if len(df) >= 0:
-        p2 = MIMEText(html, 'html')
+        p1 = MIMEText(html, 'html')
 
-        msg.attach(p2)
+        msg.attach(p1)
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
@@ -70,7 +72,7 @@ def email_send():
         server.sendmail(sender, receiver, msg.as_string())
         server.quit()
 
-##Only sends email on the weekdays
+#Only sends email on the weekdays
 day = date_today.isoweekday()
 if day != 7 and day != 6:
     email_send()

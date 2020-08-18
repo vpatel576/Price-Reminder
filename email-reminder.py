62fad0e68637bcb7ccc9fa7018fab6749b04dca7
Vrutik Patel <46287590+vpatel576@users.noreply.github.com>
2020-08-18 17:25:57 -0500

    Update email-reminder.py

--- email-reminder.py
+++ email-reminder.py
@@ -8,25 +8,23 @@ import pandas_datareader.data as web
 from datetime import datetime
 from datetime import date
 from datetime import time, timedelta
-from ima_info import targets, tickers, ima_emails
+from .ima_info import targets, tickers, ima_emails
+import config
 
 #import alpaca_trade_api as tradeapi
 
-sender = #Sender's email
-receiver =  #Receiver's email
-password = #Password
+sender = ##
+receiver = ##
+password = ##
 date_today = date.today()
 
 # Create message container - the correct MIME type is multipart/alternative.
 msg = MIMEMultipart('alternative')
-msg['Subject'] = str(date_today.month)+ '/' + str(date_today.day) + '/' +str(date_today.year)+" || Price Alert"
+msg['Subject'] =  str(date_today.month)+ '/' + str(date_today.day) + '/' +str(date_today.year)+" || Price Alert"
 msg['From'] = sender
 msg['To'] = ", ".join(receiver)
 
 # Getting all the Data
-start = date.today()-timedelta(days = 4)
-end = date.today()
-
 start = date.today()-timedelta(days = 4)
 end = date.today()
 def func(ticker, target):
@@ -45,10 +43,10 @@ def func(ticker, target):
         dict_ini[ticker[i]]['% of Target']= round(of_target*100,2)
 
     return dict_ini
-
+ 
 info = func(tickers, targets)
 
-# Preparing and cleaning data
+# Cleaning data
 df = pd.DataFrame.from_dict(info)
 df = df.transpose()
 
@@ -60,9 +58,10 @@ for i in range(len(df)):
     df.replace(df['Price'][i],'$'+str(df['Price'][i]),inplace = True)
     df.replace(df['Target'][i],'$'+str(df['Target'][i]),inplace = True)
     df.replace(df['Move Today'][i],str(df['Move Today'][i])+'%',inplace = True)
-
+    
 html = df.to_html()
 
+#Sending email
 def email_send():
     if len(df) >= 0:
         p1 = MIMEText(html, 'html')
@@ -76,7 +75,7 @@ def email_send():
         server.sendmail(sender, receiver, msg.as_string())
         server.quit()
 
-#Only sends email on the weekdays
+##Only sends email on the weekdays
 day = date_today.isoweekday()
 if day != 7 and day != 6:
-    email_send()
+  email_send()
